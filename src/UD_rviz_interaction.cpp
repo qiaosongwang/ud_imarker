2afdd92197d90e38c7a8082e6803497f89459ace
Christopher <cer@cis.udel.edu>
2013-08-05 18:35:20 -0400

    set frame_id to base_link for one of the makers (was getting error otherwise in rviz).  also put in stub of pop up menu code from interactive marker tutorial.  need to connect to actual mode changes for PCL function calls to control what ud_cursor does

--- src/UD_rviz_interaction.cpp
+++ src/UD_rviz_interaction.cpp
@@ -9,6 +9,9 @@
 #include <std_msgs/String.h>
 #include <geometry_msgs/PointStamped.h>
 
+#include <interactive_markers/interactive_marker_server.h>
+#include <interactive_markers/menu_handler.h>
+
 #include <pcl/io/pcd_io.h>
 #include <pcl/io/vtk_io.h>
 #include <pcl/ros/conversions.h>
@@ -41,6 +44,16 @@ typedef pcl::PointCloud<PointT> PointCloudT;
 
 
 using namespace std;
+using namespace visualization_msgs;
+using namespace interactive_markers;
+
+boost::shared_ptr<InteractiveMarkerServer> server;
+float marker_pos = 0;
+
+MenuHandler menu_handler;
+
+MenuHandler::EntryHandle h_first_entry;
+MenuHandler::EntryHandle h_mode_last;
 
 //changing modes
 int workmode =0; //1 polyline mode 2 box selection
@@ -68,6 +81,119 @@ vector<vector<float> > polypoints(3, vector<float>(100));
 //Declare received pointcloud from RVIZ
 pcl::PointCloud< pcl::PointXYZRGB> rviz_pt;
 pcl::PointCloud< pcl::PointXYZRGB> rviz_pt_filtered;
+
+// menu code taken from here:
+// http://ftp.isr.ist.utl.pt/pub/roswiki/rviz(2f)Tutorials(2f)Interactive(20)Markers(3a20)Getting(20)Started.html#menu
+
+void enableCb( const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
+{
+  MenuHandler::EntryHandle handle = feedback->menu_entry_id;
+  MenuHandler::CheckState state;
+  menu_handler.getCheckState( handle, state );
+
+  if ( state == MenuHandler::CHECKED )
+  {
+    menu_handler.setCheckState( handle, MenuHandler::UNCHECKED );
+    ROS_INFO("Hiding first menu entry");
+    menu_handler.setVisible( h_first_entry, false );
+  }
+  else
+  {
+    menu_handler.setCheckState( handle, MenuHandler::CHECKED );
+    ROS_INFO("Showing first menu entry");
+    menu_handler.setVisible( h_first_entry, true );
+  }
+  menu_handler.reApply( *server );
+  ros::Duration(2.0).sleep();
+  ROS_INFO("update");
+  server->applyChanges();
+}
+
+void modeCb( const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
+{
+  menu_handler.setCheckState( h_mode_last, MenuHandler::UNCHECKED );
+  h_mode_last = feedback->menu_entry_id;
+  menu_handler.setCheckState( h_mode_last, MenuHandler::CHECKED );
+
+  ROS_INFO("Switching to menu entry #%d", h_mode_last);
+
+  menu_handler.reApply( *server );
+  server->applyChanges();
+}
+
+Marker makeBox( InteractiveMarker &msg )
+{
+  Marker marker;
+
+  marker.type = Marker::CUBE;
+  marker.scale.x = msg.scale * 0.45;
+  marker.scale.y = msg.scale * 0.45;
+  marker.scale.z = msg.scale * 0.45;
+  marker.color.r = 0.5;
+  marker.color.g = 0.5;
+  marker.color.b = 0.5;
+  marker.color.a = 1.0;
+
+  return marker;
+}
+
+InteractiveMarker makeEmptyMarker( bool dummyBox=true )
+{
+  InteractiveMarker int_marker;
+  int_marker.header.frame_id = "/base_link";
+  int_marker.pose.position.y = -3.0 * marker_pos++;;
+  int_marker.scale = 1;
+
+  return int_marker;
+}
+
+void makeMenuMarker( std::string name )
+{
+  InteractiveMarker int_marker = makeEmptyMarker();
+  int_marker.name = name;
+
+  InteractiveMarkerControl control;
+
+  control.interaction_mode = InteractiveMarkerControl::MENU; // BUTTON;
+  control.always_visible = true;
+
+  control.markers.push_back( makeBox( int_marker ) );
+  int_marker.controls.push_back(control);
+
+  server->insert( int_marker );
+}
+
+void deepCb( const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
+{
+  ROS_INFO("The deep sub-menu has been found.");
+}
+
+void initMenu()
+{
+  h_first_entry = menu_handler.insert( "First Entry" );
+  MenuHandler::EntryHandle entry = menu_handler.insert( h_first_entry, "deep" );
+  entry = menu_handler.insert( entry, "sub" );
+  entry = menu_handler.insert( entry, "menu", &deepCb );
+  
+  menu_handler.setCheckState( menu_handler.insert( "Show First Entry", &enableCb ), MenuHandler::CHECKED );
+
+  MenuHandler::EntryHandle sub_menu_handle = menu_handler.insert( "Switch" );
+
+  for ( int i=0; i<5; i++ )
+  {
+    std::ostringstream s;
+    s << "Mode " << i;
+    h_mode_last = menu_handler.insert( sub_menu_handle, s.str(), &modeCb );
+    menu_handler.setCheckState( h_mode_last, MenuHandler::UNCHECKED );
+  }
+  //check the very last entry
+  menu_handler.setCheckState( h_mode_last, MenuHandler::CHECKED );
+}
+
+
+
+
+
 float calc_cp_dist(int i, vector<vector<float> > polypoints)
 {
   float cp_dist= sqrt((polypoints[0][i]-polypoints[0][i-1])*(polypoints[0][i]-polypoints[0][i-1])+(polypoints[1][i]-polypoints[1][i-1])*(polypoints[1][i]-polypoints[1][i-1])+(polypoints[2][i]-polypoints[2][i-1])*(polypoints[2][i]-polypoints[2][i-1]));
@@ -253,176 +379,174 @@ void click_segplane()
 void clickCallback(const geometry_msgs::PointStamped& msg)
 {
 
-     //Getting ROS geometry msgs
-    //ROS_INFO("New Point at X: %f, Y: %f, Z: %f\n", msg.point.x, msg.point.y, msg.point.z);
-    fp.x = msg.point.x;
-    fp.y = msg.point.y;
-    fp.z = msg.point.z;
+  //Getting ROS geometry msgs
+  //ROS_INFO("New Point at X: %f, Y: %f, Z: %f\n", msg.point.x, msg.point.y, msg.point.z);
+  fp.x = msg.point.x;
+  fp.y = msg.point.y;
+  fp.z = msg.point.z;
+  
 
-
-    // Initialize marker parameters
-    visualization_msgs::Marker points, line_strip, line_list , psphere, csphere, arrow, showtext,showtextid;
-    showtext.header.frame_id = arrow.header.frame_id=psphere.header.frame_id=csphere.header.frame_id = points.header.frame_id =     line_strip.header.frame_id = line_list.header.frame_id = "/base_link";
-    showtext.header.stamp = arrow.header.stamp=psphere.header.stamp =csphere.header.stamp= points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
-    showtext.ns = arrow.ns=psphere.ns=csphere.ns=points.ns = line_strip.ns = line_list.ns = "UD_rviz_interaction";
-    showtext.action = arrow.action=psphere.action=csphere.action=points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
-    showtext.pose.orientation.w = arrow.pose.orientation.w=psphere.pose.orientation.w =csphere.pose.orientation.w= points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
-
-    //ID
-    points.id = 0;
-    line_strip.id = 1;
-    line_list.id = 2;
-    psphere.id =3;
-    csphere.id =4;
-    arrow.id=5;
-    showtext.id=6;
-    showtextid.id=7;
-
-    //Type
-    points.type = visualization_msgs::Marker::POINTS;
-    line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-    line_list.type = visualization_msgs::Marker::LINE_LIST;
-    psphere.type = visualization_msgs::Marker::SPHERE;
-    csphere.type = visualization_msgs::Marker::SPHERE;
-    arrow.type = visualization_msgs::Marker::ARROW;
-    showtext.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-    showtextid.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-
-    //Scale
-    psphere.scale.x = 0.05*globalscale;
-    psphere.scale.y = 0.05*globalscale;
-    psphere.scale.z = 0.05*globalscale;
-
-    csphere.scale.x = 0.05*globalscale;
-    csphere.scale.y = 0.05*globalscale;
-    csphere.scale.z = 0.05*globalscale;
-
-    showtext.scale.x = 0.05*globalscale;
-    showtext.scale.y = 0.05*globalscale;
-    showtext.scale.z = 0.05*globalscale;
-
-    showtextid.scale.x = 0.05*globalscale;
-    showtextid.scale.y = 0.05*globalscale;
-    showtextid.scale.z = 0.05*globalscale;
- 
-    line_list.scale.x = 0.005*globalscale;
-    line_list.scale.y = 0.005*globalscale;
-    line_list.scale.z = 0.005*globalscale;
-
-    line_strip.scale.x = 0.005*globalscale;
-
-    //Color
-    points.color.b = 1.0;
-    points.color.a = 1.0;
-
-    // Line strip is blue
-    line_list.color.b = 1.0;
-    line_list.color.a = 1.0;
-
-    line_strip.color.b = 1.0;
-    line_strip.color.a = 1.0;
-
-    // Sphere is green
-    psphere.color.r = 1.0;
-    psphere.color.a = 0.6;
-
-    csphere.color.g = 1.0;
-    csphere.color.a = 1.0;
-
-    //Text is green
-    showtext.color.r = 1.0;
-    showtext.color.g = 1.0;
-    showtext.color.a = 1.0;
-
-    showtextid.color.r = 0.0;
-    showtextid.color.g = 1.0;
-    showtextid.color.a = 1.0;
-
-    cp.x=fp.x;
-    cp.y=fp.y;
-    cp.z=fp.z;
-
-    psphere.points.push_back(pp);
-    psphere.pose.position.x = pp.x;
-    psphere.pose.position.y = pp.y;
-    psphere.pose.position.z = pp.z;
-
-    csphere.points.push_back(cp);
-    csphere.pose.position.x = cp.x;
-    csphere.pose.position.y = cp.y;
-    csphere.pose.position.z = cp.z;
-
-
-     // Create the vertices for the points and lines
-
-    polypoints[0][polycount]=fp.x;
-    polypoints[1][polycount]=fp.y;
-    polypoints[2][polycount]=fp.z;
-
-
-       for (int i = 0; i<=polycount;i++)
-        {
-          
-           geometry_msgs::Point p;
-           p.x = polypoints[0][i];
-           p.y = polypoints[1][i];
-p.z = polypoints[2][i];
-           /*
-printf("%f\n", polypoints[0][i]);
-printf("%f\n", polypoints[1][i]);
-printf("%f\n", polypoints[2][i]);
-*/
-           points.points.push_back(p);
-           line_strip.points.push_back(p);
-
-
-            std::stringstream point_id;
-            point_id<< i << "\n";
-         
-             printf("%d\n", i);
-             
-         showtextid.text = point_id.str();
-         showtextid.pose.position.x = polypoints[0][i];
-         showtextid.pose.position.y = polypoints[1][i];
-         showtextid.pose.position.z = polypoints[2][i];
-
-          // line_list.points.push_back(p);
-          // p.z += 1.0;
-           //line_list.points.push_back(p);
-          if(i>0)
-          cpdist=cpdist+calc_cp_dist(i,polypoints);
-        }
-
-     pp.x = cp.x;
-     pp.y = cp.y;
-     pp.z = cp.z;
-
-     printf("Distance: %f \n", cpdist);
-
-     std::stringstream show_dist;
-     show_dist<< cpdist << "\n";
-
-     showtext.text = show_dist.str();
-     showtext.pose.position.x = polypoints[0][polycount];
-     showtext.pose.position.y = polypoints[1][polycount];
-     showtext.pose.position.z = polypoints[2][polycount];
-
-// Publish markers
-    marker_pub.publish(line_list);
-    marker_pub.publish(psphere);
-    marker_pub.publish(csphere);
-    marker_pub.publish(showtext);
-    marker_pub.publish(showtextid);
-    marker_pub.publish(line_strip);
-
-
-    if (polycount!=num_polypoints)
-     polycount++;
-    else
-     {
-     polycount=0;
-     cpdist=0;
-     }
+  // Initialize marker parameters
+  visualization_msgs::Marker points, line_strip, line_list , psphere, csphere, arrow, showtext,showtextid;
+  showtext.header.frame_id = arrow.header.frame_id=psphere.header.frame_id=csphere.header.frame_id = points.header.frame_id =     line_strip.header.frame_id = line_list.header.frame_id = showtextid.header.frame_id = "/base_link";
+  showtext.header.stamp = arrow.header.stamp=psphere.header.stamp =csphere.header.stamp= points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
+  showtext.ns = arrow.ns=psphere.ns=csphere.ns=points.ns = line_strip.ns = line_list.ns = "UD_rviz_interaction";
+  showtext.action = arrow.action=psphere.action=csphere.action=points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
+  showtext.pose.orientation.w = arrow.pose.orientation.w=psphere.pose.orientation.w =csphere.pose.orientation.w= points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
+  
+  //ID
+  points.id = 0;
+  line_strip.id = 1;
+  line_list.id = 2;
+  psphere.id =3;
+  csphere.id =4;
+  arrow.id=5;
+  showtext.id=6;
+  showtextid.id=7;
+  
+  //Type
+  points.type = visualization_msgs::Marker::POINTS;
+  line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+  line_list.type = visualization_msgs::Marker::LINE_LIST;
+  psphere.type = visualization_msgs::Marker::SPHERE;
+  csphere.type = visualization_msgs::Marker::SPHERE;
+  arrow.type = visualization_msgs::Marker::ARROW;
+  showtext.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+  showtextid.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+  
+  //Scale
+  psphere.scale.x = 0.05*globalscale;
+  psphere.scale.y = 0.05*globalscale;
+  psphere.scale.z = 0.05*globalscale;
+  
+  csphere.scale.x = 0.05*globalscale;
+  csphere.scale.y = 0.05*globalscale;
+  csphere.scale.z = 0.05*globalscale;
+  
+  showtext.scale.x = 0.05*globalscale;
+  showtext.scale.y = 0.05*globalscale;
+  showtext.scale.z = 0.05*globalscale;
+  
+  showtextid.scale.x = 0.05*globalscale;
+  showtextid.scale.y = 0.05*globalscale;
+  showtextid.scale.z = 0.05*globalscale;
+  
+  line_list.scale.x = 0.005*globalscale;
+  line_list.scale.y = 0.005*globalscale;
+  line_list.scale.z = 0.005*globalscale;
+  
+  line_strip.scale.x = 0.005*globalscale;
+  
+  //Color
+  points.color.b = 1.0;
+  points.color.a = 1.0;
+  
+  // Line strip is blue
+  line_list.color.b = 1.0;
+  line_list.color.a = 1.0;
+  
+  line_strip.color.b = 1.0;
+  line_strip.color.a = 1.0;
+  
+  // Sphere is green
+  psphere.color.r = 1.0;
+  psphere.color.a = 0.6;
+  
+  csphere.color.g = 1.0;
+  csphere.color.a = 1.0;
+  
+  //Text is green
+  showtext.color.r = 1.0;
+  showtext.color.g = 1.0;
+  showtext.color.a = 1.0;
+  
+  showtextid.color.r = 0.0;
+  showtextid.color.g = 1.0;
+  showtextid.color.a = 1.0;
+  
+  cp.x=fp.x;
+  cp.y=fp.y;
+  cp.z=fp.z;
+  
+  psphere.points.push_back(pp);
+  psphere.pose.position.x = pp.x;
+  psphere.pose.position.y = pp.y;
+  psphere.pose.position.z = pp.z;
+  
+  csphere.points.push_back(cp);
+  csphere.pose.position.x = cp.x;
+  csphere.pose.position.y = cp.y;
+  csphere.pose.position.z = cp.z;
+  
+  
+  // Create the vertices for the points and lines
+  
+  polypoints[0][polycount]=fp.x;
+  polypoints[1][polycount]=fp.y;
+  polypoints[2][polycount]=fp.z;
+  
+  
+  for (int i = 0; i<=polycount;i++) {
+    
+    geometry_msgs::Point p;
+    p.x = polypoints[0][i];
+    p.y = polypoints[1][i];
+    p.z = polypoints[2][i];
+    /*
+      printf("%f\n", polypoints[0][i]);
+      printf("%f\n", polypoints[1][i]);
+      printf("%f\n", polypoints[2][i]);
+    */
+    points.points.push_back(p);
+    line_strip.points.push_back(p);
+    
+    
+    std::stringstream point_id;
+    point_id<< i << "\n";
+    
+    printf("%d\n", i);
+    
+    showtextid.text = point_id.str();
+    showtextid.pose.position.x = polypoints[0][i];
+    showtextid.pose.position.y = polypoints[1][i];
+    showtextid.pose.position.z = polypoints[2][i];
+    
+    // line_list.points.push_back(p);
+    // p.z += 1.0;
+    //line_list.points.push_back(p);
+    if(i>0)
+      cpdist=cpdist+calc_cp_dist(i,polypoints);
+  }
+  
+  pp.x = cp.x;
+  pp.y = cp.y;
+  pp.z = cp.z;
+  
+  printf("Distance: %f \n", cpdist);
+  
+  std::stringstream show_dist;
+  show_dist<< cpdist << "\n";
+  
+  showtext.text = show_dist.str();
+  showtext.pose.position.x = polypoints[0][polycount];
+  showtext.pose.position.y = polypoints[1][polycount];
+  showtext.pose.position.z = polypoints[2][polycount];
+  
+  // Publish markers
+  marker_pub.publish(line_list);
+  marker_pub.publish(psphere);
+  marker_pub.publish(csphere);
+  marker_pub.publish(showtext);
+  marker_pub.publish(showtextid);
+  marker_pub.publish(line_strip);
+  
+  
+  if (polycount!=num_polypoints)
+    polycount++;
+  else {
+    polycount=0;
+    cpdist=0;
+  }
   
 }
 
@@ -438,7 +562,19 @@ int main( int argc, char** argv )
   cp.y=pp.y=0;
   cp.z=pp.z=0;
 
+  printf("ud_imarker server starting\n"); fflush(stdout);
+
   ros::init(argc, argv, "UD_rviz_interaction");
+
+  server.reset( new InteractiveMarkerServer("menu","",false) );
+  initMenu();
+
+  makeMenuMarker( "marker1" );
+  //  makeMenuMarker( "marker2" );
+
+  menu_handler.apply( *server, "marker1" );
+  //  menu_handler.apply( *server, "marker2" );
+  server->applyChanges();
 
    nhp= new ros::NodeHandle();
 
