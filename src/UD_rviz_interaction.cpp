a540d12b075f05a4d8bb9cb3c5cc27b898a7c370
Brad <brad@Barbados.(none)>
2013-08-07 15:25:23 -0400

    Added plane code

--- src/UD_rviz_interaction.cpp
+++ src/UD_rviz_interaction.cpp
@@ -68,7 +68,7 @@ MenuHandler::EntryHandle h_display_entry;
 MenuHandler::EntryHandle h_mode_last;
 
 //changing modes
-int workmode = 0; //0- distance, 1 polyline mode 2 box selection 3- selct 3 points to find a plane (BMF)
+int workmode = 0; //0- distance, 1 polyline mode 2 box selection 3- select 3 points to find a plane
 
 
 
@@ -372,7 +372,63 @@ void EstimatePlaneCb( const visualization_msgs::InteractiveMarkerFeedbackConstPt
 {
   if (ud_cursor_pts.size() < 3)
     printf("need at least 3 points to parametrize a plane\n");
+  
+  //RANSAC seems to work fine even on 3 points.  It just returns the a, b, c, and d values for the plane that is determined by them.
+  else {
+    
+  pcl::PointCloud<pcl::PointXYZ> cloud;
 
+  // Fill in the cloud data
+  cloud.width  = ud_cursor_pts.size();
+  cloud.height = 1;
+  cloud.points.resize (cloud.width * cloud.height);
+
+  for (size_t i = 0; i < cloud.points.size (); ++i)
+  {
+    cloud.points[i].x = ud_cursor_pts[i].x;
+    cloud.points[i].y = ud_cursor_pts[i].y;
+    cloud.points[i].z = ud_cursor_pts[i].z;
+  }
+  
+  std::cerr << "Point cloud data: " << cloud.points.size () << " points" << std::endl;
+  for (size_t i = 0; i < cloud.points.size (); ++i)
+    std::cerr << "    " << cloud.points[i].x << " " 
+                        << cloud.points[i].y << " " 
+                        << cloud.points[i].z << std::endl;
+
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (0.01);
+
+  seg.setInputCloud (cloud.makeShared ());
+  seg.segment (*inliers, *coefficients);
+
+  if (inliers->indices.size () == 0)
+  {
+    PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+  }
+
+  std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
+                                      << coefficients->values[1] << " "
+                                      << coefficients->values[2] << " " 
+                                      << coefficients->values[3] << std::endl;
+
+  std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
+  for (size_t i = 0; i < inliers->indices.size (); ++i)
+    std::cerr << inliers->indices[i] << "    " << cloud.points[inliers->indices[i]].x << " "
+                                               << cloud.points[inliers->indices[i]].y << " "
+                                               << cloud.points[inliers->indices[i]].z << std::endl;
+
+ 
+
+}
 }
 
 //----------------------------------------------------------------------------
