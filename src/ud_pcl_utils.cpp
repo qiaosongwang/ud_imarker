ed4c123e6e8bd7d8c09a4cf8a144023da812b9f8
Qiaosong Wang <qiaosong@gmail.com>
2013-08-15 22:10:00 -0400

    Added 3D SIFT based registration testing code
    3D SIFT keypoint based registration, use SIFT keypoints to obtain initial transformation matrix,then use ICP for precise alignment, can be used for fast registration (much faster than ICP) or recognition using pre-defined CAT models as source pointcloud
    
    Sample usage:
    sift_registration(cloud_source,cloud_target,cloud_registred)
    All inputs should be XYZRGB pointclouds
    
    Known issue:
    If there are too many sift keypoints in the source and destination pointclouds, 
    the program can not find correspondence, the error is in line 99 of  pcl/registration/impl/correspondence_rejection_sample_consensus.hpp

--- src/ud_pcl_utils.cpp
+++ src/ud_pcl_utils.cpp
@@ -635,6 +635,211 @@ void segment_color(pcl::PointCloud<pcl::PointXYZRGB> & cloud,
     }
 }
 
+//----------------------------------------------------------------------------
+//SIFT keypoint detection
+
+void sift_detection ( pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr input,
+                       pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints)
+{
+
+    pcl::SIFTKeypoint<pcl::PointXYZRGB, pcl::PointXYZI>* sift3D = new pcl::SIFTKeypoint<pcl::PointXYZRGB, pcl::PointXYZI>;
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB> ());
+    sift3D->setScales(0.1, 3, 2);
+    sift3D->setMinimumContrast(0.0);
+    sift3D->setSearchMethod(tree);
+    sift3D->setInputCloud(input);
+    sift3D->compute(*keypoints);
+}
+
+
+//----------------------------------------------------------------------------
+//FPFH feature detection
+
+void fpfh_detection ( pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr input,
+                          pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints,
+                          pcl::PointCloud<pcl::FPFHSignature33>::Ptr features)
+{
+    pcl::Feature<pcl::PointXYZRGB, pcl::FPFHSignature33>::Ptr feature_extractor_ (new pcl::FPFHEstimationOMP<pcl::PointXYZRGB, pcl::Normal, pcl::FPFHSignature33>);
+    feature_extractor_->setSearchMethod (pcl::search::Search<pcl::PointXYZRGB>::Ptr (new pcl::search::KdTree<pcl::PointXYZRGB>));
+    feature_extractor_->setRadiusSearch (50);
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr kpts(new pcl::PointCloud<pcl::PointXYZRGB>);
+    kpts->points.resize(keypoints->points.size());
+
+    pcl::copyPointCloud(*keypoints, *kpts);
+
+    pcl::FeatureFromNormals<pcl::PointXYZRGB, pcl::Normal, pcl::FPFHSignature33>::Ptr feature_from_normals = boost::dynamic_pointer_cast<pcl::FeatureFromNormals<pcl::PointXYZRGB, pcl::Normal, pcl::FPFHSignature33> > (feature_extractor_);
+    feature_extractor_->setInputCloud(kpts);
+   //Normals estimation and Descriptors extraction
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new  pcl::PointCloud<pcl::Normal>);
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_estimation;
+    normal_estimation.setSearchMethod (pcl::search::Search<pcl::PointXYZRGB>::Ptr (new pcl::search::KdTree<pcl::PointXYZRGB>));
+    normal_estimation.setRadiusSearch (50);
+    normal_estimation.setInputCloud (kpts);
+    normal_estimation.compute (*normals);
+    feature_from_normals->setInputNormals(normals);
+    feature_extractor_->compute (*features);
+
+}
+
+//----------------------------------------------------------------------------
+
+//Find feature correspondence
+void find_correspondences (
+        pcl::PointCloud<pcl::FPFHSignature33>::Ptr source,
+        pcl::PointCloud<pcl::FPFHSignature33>::Ptr target,
+        vector<int>& correspondences)
+{
+
+    correspondences.resize (source->size());
+
+ 
+    pcl::KdTreeFLANN<pcl::FPFHSignature33> descriptor_kdtree;
+    descriptor_kdtree.setInputCloud (target);
+
+    const int k = 1;
+    vector<int> k_indices (k);
+    vector<float> k_squared_distances (k);
+    for (size_t i = 0; i < source->size (); ++i)
+    {
+        descriptor_kdtree.nearestKSearch (*source, i, k, k_indices, k_squared_distances);
+        correspondences[i] = k_indices[0];
+    }
+}
+
+//----------------------------------------------------------------------------
+void filter_correspondences ( pcl::PointCloud<pcl::PointXYZI>::Ptr source_keypoints_,
+                             pcl::PointCloud<pcl::PointXYZI>::Ptr target_keypoints_ ,
+                             vector<int>& source2target_ , vector<int>& target2source_ ,
+                             pcl::CorrespondencesPtr correspondences_)
+{
+    vector<pair<unsigned, unsigned> > correspondences;
+    for (unsigned cIdx = 0; cIdx < source2target_.size (); ++cIdx)
+        if (target2source_[source2target_[cIdx]] == cIdx)
+            correspondences.push_back(make_pair(cIdx, source2target_[cIdx]));
+
+    correspondences_->resize (correspondences.size());
+    for (unsigned cIdx = 0; cIdx < correspondences.size(); ++cIdx)
+    {
+        (*correspondences_)[cIdx].index_query = correspondences[cIdx].first;
+        (*correspondences_)[cIdx].index_match = correspondences[cIdx].second;
+    }
+
+    pcl::registration::CorrespondenceRejectorSampleConsensus<pcl::PointXYZI> rejector;
+    rejector.setInputCloud(source_keypoints_);
+    rejector.setTargetCloud(target_keypoints_);
+    rejector.setMaxIterations(20);
+    rejector.setInlierThreshold(2);
+    rejector.setInputCorrespondences(correspondences_);
+    rejector.getCorrespondences(*correspondences_);
+}
+
+//----------------------------------------------------------------------------
+//Keypoint based initial transform
+Eigen::Matrix4f initial_transform (
+        pcl::PointCloud<pcl::PointXYZI>::Ptr source_keypoints_ ,
+        pcl::PointCloud<pcl::PointXYZI>::Ptr target_keypoints_ ,
+        pcl::CorrespondencesPtr correspondences_
+        )
+{
+    Eigen::Matrix4f initial_transformation_matrix_ = Eigen::Matrix4f::Identity();
+    pcl::registration::TransformationEstimation<pcl::PointXYZI, pcl::PointXYZI>::Ptr transformation_estimation (new pcl::registration::TransformationEstimationSVD<pcl::PointXYZI, pcl::PointXYZI>);
+    transformation_estimation->estimateRigidTransformation (*source_keypoints_, *target_keypoints_, *correspondences_, initial_transformation_matrix_);
+    return initial_transformation_matrix_;
+}
+
+//----------------------------------------------------------------------------
+
+//ICP final refinement
+Eigen::Matrix4f final_transform (
+        pcl::PointCloud<pcl::PointXYZI>::Ptr source_transformed_ ,
+        pcl::PointCloud<pcl::PointXYZI>::Ptr target_segmented_
+        )
+{
+  
+    Eigen::Matrix4f final_transformation_matrix_=Eigen::Matrix4f::Identity ();
+    pcl::Registration<pcl::PointXYZI, pcl::PointXYZI>::Ptr registration (new pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>);
+    registration->setInputCloud(source_transformed_);
+    registration->setInputTarget (target_segmented_);
+    registration->setMaxCorrespondenceDistance(0.01); //4
+    registration->setRANSACOutlierRejectionThreshold (1000); //4
+    registration->setTransformationEpsilon (0.0001); //0.0001
+    registration->setRANSACIterations(100);
+    //registration->setMaximumIterations (10000); //1
+    registration->align(*source_transformed_);
+    final_transformation_matrix_ = registration->getFinalTransformation();
+    return final_transformation_matrix_;
+}
+
+//----------------------------------------------------------------------------
+
+//3D SIFT keypoint based registration, use SIFT keypoints to obtain initial transformation matrix,
+//Then use ICP for precise alignment, can be used for fast registration (much faster than ICP
+//or recognition using pre-defined CAT models as source pointcloud
+
+//Sample usage:
+//sift_registration(cloud_source,cloud_target,cloud_registred)
+//All inputs should be XYZRGB pointclouds
+
+//Known issue:
+//If there are too many sift keypoints in the source and destination pointclouds, 
+//the program can not find correspondence, the error is in line 99 of 
+//pcl/registration/impl/correspondence_rejection_sample_consensus.hpp
+
+
+bool sift_registration (
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr source ,
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr target,
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr registered)
+{
+
+    //sift3d keypoint detector
+    pcl::PointCloud<pcl::PointXYZI>::Ptr source_keypoints (new pcl::PointCloud<pcl::PointXYZI> ());
+    pcl::PointCloud<pcl::PointXYZI>::Ptr target_keypoints (new pcl::PointCloud<pcl::PointXYZI> ());
+    sift_detection (source, source_keypoints);
+    sift_detection (target, target_keypoints);
+
+    // For testing
+    // pcl::io::savePCDFileBinary("./target_keypoints.pcd",*target_keypoints);
+    
+
+    //FPFH feature extractor
+    pcl::PointCloud<pcl::FPFHSignature33>::Ptr source_features (new pcl::PointCloud<pcl::FPFHSignature33>);
+    pcl::PointCloud<pcl::FPFHSignature33>::Ptr target_features (new pcl::PointCloud<pcl::FPFHSignature33>);
+    fpfh_detection (source, source_keypoints, source_features);
+    fpfh_detection (target, target_keypoints, target_features);
+    // For testing
+    //pcl::io::savePCDFileBinary("./target_features.pcd",*target_features);
+  
+
+    //Find Correspondences
+    vector<int> source2target ;
+    vector<int> target2source ;
+    find_correspondences (source_features, target_features, source2target);
+    find_correspondences (target_features, source_features, target2source);
+
+    //Filter Correnspondences
+      pcl::CorrespondencesPtr correspondences (new pcl::Correspondences);
+      filter_correspondences (source_keypoints, target_keypoints , source2target , target2source, correspondences);
+
+    //Initial transformation
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr source_transformed (new pcl::PointCloud<pcl::PointXYZRGB>);
+    Eigen::Matrix4f initial_transformation_matrix = initial_transform (source_keypoints, target_keypoints, correspondences );
+   //Initial alignment
+    pcl::transformPointCloud(*source, *source_transformed, initial_transformation_matrix);
+
+   //Final ICP alignment
+    //Eigen::Matrix4f final_transformation_matrix = final_transform (source_transformed , registered, target);
+    pcl::copyPointCloud(*source_transformed, *registered);  
+    
+	 //For testing
+	 //pcl::io::savePCDFileBinary("./source.pcd",*source);
+	 //pcl::io::savePCDFileBinary("./target.pcd",*target);
+	 //pcl::io::savePCDFileBinary("./registered.pcd",*registered);
+   return true;
+}
+
+
 
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
